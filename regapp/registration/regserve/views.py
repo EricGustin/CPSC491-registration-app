a9447bc86d15a229193f74ecaab733f08bab15b1
Eric Gustin <ericgustin44@gmail.com>
2021-11-16 18:00:12 -0800

    Git add hello world

--- regapp/registration/regserve/views.py
+++ regapp/registration/regserve/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views (handlers) here.
+
+def index(request):
+    return HttpResponse("Response from Django backend.")
