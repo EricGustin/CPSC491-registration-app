e846a419eda0c4e87ff4d2d504396bc924c56d02
Eric Gustin <ericgustin44@gmail.com>
2021-11-18 15:44:58 -0800

    Add test

--- regapp/registration/regserve/tests.py
+++ regapp/registration/regserve/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.test_client = Client()
+
+    def test_response(self):
+        response = self.test_client.get("/regserve")
+        print(f"Inside hello world test, response is {response}.")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Response from Django backend.")
--- regapp/registration/regserve/views.py
+++ regapp/registration/regserve/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views (handlers) here.
+# views (handlers) go here.
 
 def index(request):
     return HttpResponse("Response from Django backend.")
