d0e28bd387243481715f64c02bb08f184d82f76f
Eric Gustin <ericgustin44@gmail.com>
2021-11-23 14:07:21 -0800

    Add checkpoint

--- regapp/registration/regserve/models.py
+++ regapp/registration/regserve/models.py
@@ -1,3 +1,6 @@
+from django.core import validators
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Student(models.Model):
+    studentid = models.PositiveIntegerField(blank=True, validators={MinValueValidator(1)})
